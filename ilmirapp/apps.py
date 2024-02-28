0c0654acfad6af7c40af17d5ab1ffd9085d257cb
iamlilya <kazyhanovalilia@gmail.com>
2024-02-28 17:32:44 +0500

    основа

---
+++ ilmirapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IlmirappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ilmirapp'
---
+++ ilmirapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+ 
+def index(request):
+    data = {"header": "Hello Django", "message": "Welcome to Python"}
+    return render(request, "index.html", context=data)
