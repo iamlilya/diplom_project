90db051c1355ba5c3551f9cce828fddb2a060bfa
iamlilya <kazyhanovalilia@gmail.com>
2024-03-02 16:35:19 +0500

    Создание endpoint Gradient

--- mag_project/urls.py
+++ mag_project/urls.py
@@ -1,52 +1,21 @@
-"""
-URL configuration for mag_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-
-from django.contrib.auth.models import User
 from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
+
 from ilmirapp import views
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide a way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+class Gradient(APIView):
+    def get(self, request):
+        print(request.GET)
+        latitude = request.GET['latitude']
+        longitude = request.GET['longitude']
+        response = "latitude: " + latitude + "; longitude: " + longitude
+        return Response(response)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", views.index),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/gradient/', Gradient.as_view())
 ]
